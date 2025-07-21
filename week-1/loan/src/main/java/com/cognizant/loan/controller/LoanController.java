a3affd17c24212434591fb7cdd9bf032a164c52b
Aneesha <aneeshavarma07@gmail.com>
2025-07-21 11:34:36 +0530

    week-5

---
+++ week-1/loan/src/main/java/com/cognizant/loan/controller/LoanController.java
@@ -0,0 +1,14 @@
+package com.cognizant.loan.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class LoanController {
+
+    @GetMapping("/loans/{number}")
+    public Loan getLoan(@PathVariable String number) {
+        return new Loan(number, "car", 400000, 3258, 18);
+    }
+
+    record Loan(String number, String type, double loan, double emi, int tenure) {}
+}
